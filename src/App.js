243d55e58c3c3498543e882bd321edda979e62b6
EFAREM <github.fictionalised@dralias.com>
2024-05-24 13:31:15 +0100

    Persona Test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import PersonaReact from 'persona-react';
+import QRCode from "react-qr-code";
+
 import './App.css';
+
+const TID="itmpl_iysj7t8MNyLvRWPBP1NoMEJewjRS";
+const IID="inq_DGtZQbGEPWwvt2dp4Kj2rJb5BHs5";
+const EID="env_FZvJki2dGQ3ngyMpgoWYBZyGCiQu"
 
 function App() {
   return (
-    <div className="App"><p>Hello, World!</p></div>
+    <div className="App">
+      <PersonaReact
+      inquiryId={IID}
+      environmentId={EID}
+      onComplete={({ inquiryId, status, fields }) => {
+        // Inquiry completed. Optionally tell your server about it.
+        console.log(`Sending finished inquiry ${inquiryId} to backend`);
+      }}
+    />
+    <QRCode value={`https://www.skellige.xyz?inquiry-template-id=${TID}&environment-id=${EID}`} />
+    </div>
   );
 }
 
